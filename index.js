993565209c5034aa7b2b288229cd5d2da94aaf89
MidnightOwll <dv92040@gmail.com>
2021-07-26 13:41:51 +0300

    first

---
+++ index.js
@@ -0,0 +1,38 @@
+const path = require('path')
+const url = require('url')
+const {app, BrowserWindow} = require('electron')
+const { request } = require('https')
+require(__dirname + '/request.js')
+
+let win
+
+function createWindow() {
+    win = new BrowserWindow({
+        width: 900, 
+        height: 600, 
+        icon: __dirname + '/img/icon.png'
+    })
+
+    
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true
+    }))
+
+    
+
+    win.webContents.openDevTools();
+
+    win.on('closed', () => {
+        win = null
+    })
+}
+
+
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+    app.quit()
+})
---
+++ request.js
@@ -0,0 +1,12 @@
+// async function request(){
+//     let url = 'https://api.mangadex.org/manga'
+//     let response = await fetch(url)
+    
+//     let mangaList = await response.json() // читаем ответ в формате JSON
+    
+//     console.log(mangaList)  
+// }
+
+
+
+
